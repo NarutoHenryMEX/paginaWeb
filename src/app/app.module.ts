ba9daf5f86d31898a131abd451b56b331c8d5ca4
NarutoHenryMEX <henry_portador@outlook.com>
2019-12-31 19:40:42 -0600

    Creación de componentes, crud, home, login, navbar y notfound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { NotFounPageComponent } from './componentes/not-foun-page/not-foun-page.component';
+import { CrudPageComponent } from './componentes/crud-page/crud-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    NotFounPageComponent,
+    CrudPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/crud-page/crud-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-page',
+  templateUrl: './crud-page.component.html',
+  styleUrls: ['./crud-page.component.scss']
+})
+export class CrudPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
