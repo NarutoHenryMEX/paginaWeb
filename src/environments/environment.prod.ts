aa76011faf5e9d9bb445eb75ebcd3357f2c67e29
NarutoHenryMEX <henry_portador@outlook.com>
2020-01-01 17:15:35 -0600

    Se agregaron datos de firebase en environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfing:{
+    apiKey: "AIzaSyC2qhtk2sDW_UbkXJaOmb8zbgRDMyvVuuo",
+    authDomain: "inmobiliaria-f3d75.firebaseapp.com",
+    databaseURL: "https://inmobiliaria-f3d75.firebaseio.com",
+    projectId: "inmobiliaria-f3d75",
+    storageBucket: "inmobiliaria-f3d75.appspot.com",
+    messagingSenderId: "669063688110",
+    appId: "1:669063688110:web:6313386f579fe474f7623f",
+    measurementId: "G-5KH15SEQKL"
+  }
 };
