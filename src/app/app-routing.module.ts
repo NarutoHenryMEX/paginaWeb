2ca53e6d0c379ee55a2ff32d72611aa48a5920c6
NarutoHenryMEX <henry_portador@outlook.com>
2020-01-01 14:23:02 -0600

    Se agrego diseño de bootstrap a navbar, se editaron las opciones, se hizo direccionamiento de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { CrudPageComponent } from './componentes/crud-page/crud-page.component';
+import { NoFoundPageComponent } from './componentes/no-found-page/no-found-page.component';
 
+const routes: Routes = [
+{path: '', component: HomePageComponent},
+{path: 'loginx', component: LoginPageComponent},
+{path: 'crud', component: CrudPageComponent},
+{path: '**', component: NoFoundPageComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
