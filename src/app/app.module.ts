ff5bd16e6e2fe49f5f029e2d4707a33c7ca4c580
NarutoHenryMEX <henry_portador@outlook.com>
2020-01-01 19:43:14 -0600

    Instalamos servicios de Autenticacion, se crearon metodos para inicio de sesion y se redirecciono a la pagina deseada cuando se inicia sesion correctamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './componentes/home-page/home-page.component';
@@ -8,6 +8,13 @@ import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { LoginPageComponent } from './componentes/login-page/login-page.component';
 import { CrudPageComponent } from './componentes/crud-page/crud-page.component';
 import { NoFoundPageComponent } from './componentes/no-found-page/no-found-page.component';
+
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+
+import { environment } from '../environments/environment'; 
+import { AuthService} from './servicios/auth.service';
+
 
 
 @NgModule({
@@ -17,13 +24,16 @@ import { NoFoundPageComponent } from './componentes/no-found-page/no-found-page.
     NavbarComponent,
     LoginPageComponent,
     CrudPageComponent,
-    NoFoundPageComponent,
+    NoFoundPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfing)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService} from '../../servicios/auth.service';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+public email: string;
+public pass: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    
   }
 
+  onSubmitLoginUser(){
+    this.authService.loginUser(this.email, this.pass)
+    .then((res) => {
+     this.router.navigate(['/crud']);
+    }).catch((err) => {
+      console.log(err);
+      this.router.navigate(['**'])
+    });
+  }
+  
+  
 }
